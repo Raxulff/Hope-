5e07ebee04b374dd142dbae4b0292996b02daddc
S.Rahul Raj <24ee1453@stjosephstechnology.ac.in>
2026-03-24 13:35:15 +0530

    Update Leetcode_172.java

--- DAY_3/Leetcode_172.java
+++ DAY_3/Leetcode_172.java
@@ -1,1 +1,20 @@
 
+class Solution {
+    public int trailingZeroes(int n) {
+        // number of five
+        // if(n < 5) return 0;
+        // int count = 0;
+        // for(int itr = 5;itr <= n;itr++){
+        //     if(itr%5 == 0){
+        //         count = count + itr/5;
+        //     }
+        // }
+        int count = 0;
+        while(n>0){
+           
+            count+=n/5;
+             n/=5;
+        }
+        return count;
+    }
+}
