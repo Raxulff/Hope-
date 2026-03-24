6fda0aa3718f05aaa24925052f3f7f2f0ac4fbbe
S.Rahul Raj <24ee1453@stjosephstechnology.ac.in>
2026-03-24 13:14:34 +0530

    Update Calculator.java

--- DAY_2/Calculator.java
+++ DAY_2/Calculator.java
@@ -1,1 +1,30 @@
 
+import java.util.*;
+public class Calculator {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        char ch = sc.next().charAt(0);
+        int a = sc.nextInt();
+        int b = sc.nextInt();   
+        switch(ch){
+            case '+':
+                System.out.println(a+b);
+                break;
+            case '-':
+                System.out.println(a-b);
+                break;
+            case '*':
+                System.out.println(a*b);
+                break;
+            case '/':
+                if(b!=0){
+                    System.out.println(a/b);
+                }else{
+                    System.out.println("Cannot divide by zero");
+                }
+                break;
+            default:
+                System.out.println("Invalid operator");
+        }
+    }
+}
