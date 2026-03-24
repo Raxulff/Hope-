f3e7c677ee4aba3fb7ffcbf1f8444d66fb8014c4
S.Rahul Raj <24ee1453@stjosephstechnology.ac.in>
2026-03-24 09:23:19 +0530

    Implement maximumWealth method to calculate max wealth
    Fix the loop to iterate through columns and rows correctly.

--- DAY_1/Leetcode_1672.java
+++ DAY_1/Leetcode_1672.java
@@ -1,1 +1,16 @@
 
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+        int R = accounts.length;
+        int C = accounts[0].length;
+        int max = Integer.MIN_VALUE;
+        for(int row = 0;row < R;row++){
+            int sum = 0;
+            for(int col = 0;col <C;col++){
+                sum+=accounts[row][col];
+            }
+            max = Math.max(sum,max);
+        }
+        return max;
+    }
+}
