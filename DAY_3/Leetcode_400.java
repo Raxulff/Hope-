ae0d58979bbb2015dbb6b17089a1a0a1e1705b31
S.Rahul Raj <24ee1453@stjosephstechnology.ac.in>
2026-03-24 13:49:42 +0530

    Update Leetcode_400.java

--- DAY_3/Leetcode_400.java
+++ DAY_3/Leetcode_400.java
@@ -1,1 +1,21 @@
 
+class Solution {
+    public int findNthDigit(int n) {
+        
+        long count = 1L;
+        long digit = 9L;
+        long start = 1L;
+        while(n > digit*count){
+            n -= digit*count;
+            //count
+            count++;
+            //digit 
+            digit = digit*10;
+            start = start*10;
+        }
+        //exact digit
+        start = start + (n-1)/count;
+        String num = Long.toString(start);
+        return num.charAt((int) ((n-1)%count)) - '0';
+    }
+}
