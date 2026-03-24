b277ccab84bd287c3af3409596c024f274052942
S.Rahul Raj <24ee1453@stjosephstechnology.ac.in>
2026-03-24 13:33:44 +0530

    Update Leetcode_268.java

--- DAY_2/Leetcode_268.java
+++ DAY_2/Leetcode_268.java
@@ -1,1 +1,11 @@
 
+class Solution {
+    public int missingNumber(int[] nums) {
+        int sum = 0;
+        for(int val : nums){
+            sum+=val;
+        }
+        int n = nums.length;
+        return n*(n+1)/2 - sum;
+    }
+}
