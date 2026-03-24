a55dbb6f77c16b5724543d7ecc8345b5d53fa8f2
S.Rahul Raj <24ee1453@stjosephstechnology.ac.in>
2026-03-24 13:39:34 +0530

    Implement isPowerOfFour method in Solution class

--- DAY_3/Leetcode_342.java
+++ DAY_3/Leetcode_342.java
@@ -1,1 +1,7 @@
 
+class Solution {
+    public boolean isPowerOfFour(int n) {
+        // return (n&(n-1)) == 0 && (n-1)%3 == 0 && n > 0; 
+        return (n&(0x55555555)) != 0 && (n&(n-1)) == 0;
+    }
+}
