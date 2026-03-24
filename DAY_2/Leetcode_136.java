f54fb4c093a020df065e73e07ca7627cfa6411d1
S.Rahul Raj <24ee1453@stjosephstechnology.ac.in>
2026-03-24 13:27:15 +0530

    Update Leetcode_136.java

--- DAY_2/Leetcode_136.java
+++ DAY_2/Leetcode_136.java
@@ -1,1 +1,10 @@
 
+class Solution {
+    public int singleNumber(int[] nums) {
+        int xor = 0;
+        for(int ind = 0;ind<nums.length;ind++){
+            xor = xor^nums[ind];
+        }
+        return xor;
+    }
+}
